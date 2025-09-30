c08e6232b30b26a3dbef4d97baba4a86c4fd229f
Tarang Kalariya <tarang.appliquer@gmail.com>
2025-09-30 18:19:05 +0530

    added @t3-oss/env-nextjs

---
+++ src/env/server.ts
@@ -0,0 +1,9 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    AUTH_SECRET: z.string().min(1),
+  },
+  experimental__runtimeEnv: process.env
+});
