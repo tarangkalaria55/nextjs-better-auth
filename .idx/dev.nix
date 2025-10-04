ddaf31b841c8f01ec18f1f914252049da1731f35
tarangkalaria55 <tarangkalaria55@gmail.com>
2025-10-04 12:44:46 +0000

    nix setup

---
+++ .idx/dev.nix
@@ -0,0 +1,36 @@
+{pkgs}: {
+  channel = "stable-24.05";
+  packages = [
+    pkgs.nodejs_20
+  ];
+  idx.extensions = [
+    "biomejs.biome"
+		"mikestead.dotenv"
+		"dsznajder.es7-react-js-snippets"
+		"YoavBls.pretty-ts-errors"
+		"bradlc.vscode-tailwindcss"
+		"johnpapa.vscode-peacock"
+		"christian-kohler.path-intellisense"
+		"aaron-bond.better-comments"
+		"steoates.autoimport"
+		"formulahendry.auto-complete-tag"
+		"mhutchie.git-graph"
+  ];
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "npm"
+          "run"
+          "dev"
+          "--"
+          "--port"
+          "$PORT"
+          "--hostname"
+          "0.0.0.0"
+        ];
+        manager = "web";
+      };
+    };
+  };
+}
